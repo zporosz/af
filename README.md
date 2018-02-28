6f8b5ee655fd530b400a25c09966ed2cd4cbfc26
Orosz Zoltan <35689482+zporosz@users.noreply.github.com>
2018-02-28 11:22:56 +0100

    status 28 Feb 2018
    This point is ready:
    - This is a scrolling webpage
    - Backgrounds was created
    - Animated menus with jQuery
    - First slideshow was created for gallerys by w3scgool
    - The gallerys show and hide function is running

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# af
-the page for present the alex francis hobby photographer's pictures
+This site presents a hungarian amateur photographer, Alex Francis's photos. 
+
+The webpage is a scrolling site. I used an animate function from jQuery. Here is the sorce: http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js
+
+1st step was the created backrounds, menu and main pege's titles. The next task is to create a photogallery.
--- scripts.js
+++ scripts.js
@@ -1,15 +1,17 @@
-﻿$(document).ready(function(){
+﻿//This code from jQuery
 
-  // hide #back-top first
-  $(".home").hide();
+$(document).ready(function(){
+
+  // hide Home first
+  $(".menuHome").hide();
 	
-  // fade in #back-top
+  // fade in Home
   $(function () {
     $(window).scroll(function () {
       if ($(this).scrollTop() > 100) {
-        $('.home').fadeIn();
+        $('.menuHome').fadeIn();
       } else {
-        $('.home').fadeOut();
+        $('.menuHome').fadeOut();
       }
     });
 
@@ -27,7 +29,7 @@
 $("document").ready(function() {
   
   // Home button
-  $('.home').click(function(){
+  $('.menuHome').click(function(){
     $('html, body').animate({
       scrollTop: $(".page1").offset().top
     }, 1200);				   
@@ -52,6 +54,96 @@ $("document").ready(function() {
     $('html, body').animate({
       scrollTop: $(".separator3").offset().top
     }, 1200);				   
+  }); 
+});
+
+$(document).ready(function(){
+
+  // hide Gallery (Page 2) menu
+  $(".menuPage2").hide();
+	
+  // fade in Home
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > $(".page2").offset().top - 500) {
+        $('.menuPage2').fadeIn();
+      } else {
+        $('.menuPage2').fadeOut();
+      }
+    });
+  });      
+  
+  
+  // show and hide the gallery by the menu click
+ $( ".menuBp" ).click(function() {
+   
+   
+   // Pushed the 'Budapest' button
+   // Show: Budapest, Hide: Madarak, Tájképek
+    $( ".galleryPageBp" ).animate({ "margin-left": "0" }, "slow" ); 
+    $( ".galleryPageBp" ).css( "visibility", "visible" );
+    $( ".menuBp" ).css( "color", "blue" );  
+   
+    $( ".galleryPageBirds" ).animate({ "margin-left": "100%" }, "slow" );
+    $( ".galleryPageBirds" ).css( "visibility", "hidden" );
+    $( ".menuBirds" ).css( "color", "rgba(0, 0, 0, 0.8)" );
+   
+    $( ".galleryPageNatural" ).animate({ "margin-left": "100%" }, "slow" );
+    $( ".galleryPageNatural" ).css( "visibility", "hidden" );
+    $( ".menuNatural" ).css( "color", "rgba(0, 0, 0, 0.8)" );
   });
   
+  // Pushed the 'Madarak' button
+   // Show: Madarak, Hide: Budapest, Természet
+  $( ".menuBirds" ).click(function() {
+    $( ".galleryPageBp" ).animate({ "margin-left": "100%" }, "slow" );
+    $( ".galleryPageBp" ).css( "visibility", "hidden" );
+    $( ".menuBp" ).css( "color", "rgba(0, 0, 0, 0.8)" );
+    
+    $( ".galleryPageBirds" ).animate({ "margin-left": "0" }, "slow" );
+    $( ".galleryPageBirds" ).css( "visibility", "visible" );
+    $( ".menuBirds" ).css( "color", "blue" );
+    
+    $( ".galleryPageNatural" ).animate({ "margin-left": "100%" }, "slow" );
+    $( ".galleryPageNatural" ).css( "visibility", "hidden" );
+    $( ".menuNatural" ).css( "color", "rgba(0, 0, 0, 0.8)" );
+  });
+  
+  // Pushed the 'Természet' button
+   // Show: Természet, Hide: Budapest, Madarak
+  $( ".menuNatural" ).click(function() {
+    $( ".galleryPageBp" ).animate({ "margin-left": "100%" }, "slow" );
+    $( ".galleryPageBp" ).css( "visibility", "hidden" );
+    $( ".menuBp" ).css( "color", "rgba(0, 0, 0, 0.8)" );
+    
+    $( ".galleryPageBirds" ).animate({ "margin-left": "100%" }, "slow" );
+    $( ".galleryPageBirds" ).css( "visibility", "hidden" );
+    $( ".menuBirds" ).css( "color", "rgba(0, 0, 0, 0.8)" );
+    
+    $( ".galleryPageNatural" ).animate({ "margin-left": "0" }, "slow" );
+    $( ".galleryPageNatural" ).css( "visibility", "visible" );
+    $( ".menuNatural" ).css( "color", "blue" );
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
