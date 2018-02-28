c4d66d1ad23cd1b0264a6b53cf668b82433aab9b
Orosz Zoltan <35689482+zporosz@users.noreply.github.com>
2018-02-28 11:10:11 +0100

    status - 28 Feb 2018
    This point is ready:
    - This is a scrolling webpage
    - Backgrounds was created
    - Animated menus with jQuery
    - First slideshow was created for gallerys by w3scgool
    - The gallerys show and hide function is running

---
+++ readme.rd
@@ -0,0 +1,7 @@
+
+This project introduces Alex Francis's photos
+
+All photos on this site are owned by Alex Francis.
+Copying and downloading without permission is forbidden!
+
+The website was created by Orosz Zoltan - oz
---
+++ scripts.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(function(){
+
+  // hide #back-top first
+  $(".home").hide();
+	
+  // fade in #back-top
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('.home').fadeIn();
+      } else {
+        $('.home').fadeOut();
+      }
+    });
+
+    // scroll body to 0px on click
+      $('.home a').click(function () {
+        $('body,html').animate({
+          scrollTop: $(".page1").offset().top
+        }, 1000);
+          return false;
+        });
+  });      
+});
+
+// Menu controlers
+$("document").ready(function() {
+  
+  // Home button
+  $('.home').click(function(){
+    $('html, body').animate({
+      scrollTop: $(".page1").offset().top
+    }, 1200);				   
+  });
+  
+  // Gallery button
+  $('.menuGallery').click(function(){
+    $('html, body').animate({
+      scrollTop: $(".separator1").offset().top
+    }, 1200);				   
+  });
+  
+  // Photographer button
+  $('.menuPhotographer').click(function(){
+    $('html, body').animate({
+      scrollTop: $(".separator2").offset().top
+    }, 1200);				   
+  });
+  
+  // Contact button
+  $('.menuContact').click(function(){
+    $('html, body').animate({
+      scrollTop: $(".separator3").offset().top
+    }, 1200);				   
+  });
+  
+});
